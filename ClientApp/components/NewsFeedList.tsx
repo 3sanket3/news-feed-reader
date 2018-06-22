74006e0948063af88c3e79e05c224b11bc4ab2be
Sanket Patel <3sanket3@gmail.com>
2018-06-22 18:06:59 +0530

    add search bar on home

--- ClientApp/components/NewsFeedList.tsx
+++ ClientApp/components/NewsFeedList.tsx
@@ -1,53 +1,96 @@
-import * as React from 'react';
-import { RouteComponentProps } from 'react-router';
-import { NewsFeedItem } from './NewsFeedItem';
+import * as React from "react";
+import { RouteComponentProps } from "react-router";
+import { NewsFeedItem } from "./NewsFeedItem";
 
-export class NewsFeedList extends React.Component<any & RouteComponentProps<{}>, {}> {
+export class NewsFeedList extends React.Component<
+  any & RouteComponentProps<{}>,
+  {}
+> {
+  static PropTypes = {
+    onlySubscribed: React.PropTypes.bool,
+    providerId: React.PropTypes.number,
+    renderIfNoItemsFound: React.PropTypes.func
+  };
 
-    
-    static PropTypes ={
-        onlySubscribed: React.PropTypes.bool,
-        providerId: React.PropTypes.number,
-        renderIfNoItemsFound: React.PropTypes.func
+  state = {
+    feedItmes: [],
+    apiResponded: false, /*renderIfNoItemsFound should not be called if api is yet to respond */
+    searchTerm: ''
+  };
+
+  backupFeeds : Array<any> =[];
+  componentDidMount() {
+    console.log("onlySubscribed " + this.props.onlySubscribed);
+    this.getAllFeeds();
+  }
+
+  getAllFeeds = () => {
+    let url = this.props.onlySubscribed
+      ? "api/News/Get?onlySubscribed=true"
+      : "api/News/Get";
+
+    if (this.props.providerId > 0) {
+      url = "api/News/Get?providerId=" + this.props.providerId;
+    } else if (this.props.onlySubscribed) {
+      url = "api/News/Get?onlySubscribed=true";
+    } else {
+      url = "api/News/Get";
     }
 
-    state = { feedItmes: [], apiResponded : false /*renderIfNoItemsFound should not be called if api is yet to respond */ };
-    
-    componentDidMount() {
-        console.log("onlySubscribed "+this.props.onlySubscribed);
-        this.getAllFeeds();
-    }
+    fetch(url)
+      .then(response => response.json() as Promise<any[]>)
+      .then(data => {
+          this.setState({ feedItmes: data, apiResponded: true });
+          this.backupFeeds = [...this.state.feedItmes];
+      });
+  };
 
-    getAllFeeds = () => {
+  handleChange = (event : any)=>{
+    this.setState({searchTerm: event.target.value});
+    let searchedItems = this.backupFeeds.filter(feed=> (feed.title as string).toLowerCase().indexOf(this.state.searchTerm.toLowerCase())>=0);
+    this.setState({feedItmes: searchedItems});
+  }
+  handleSubmit = (event :any)=>{
+    let searchedItems = this.backupFeeds.filter(feed=> (feed.title as string).toLowerCase().indexOf(this.state.searchTerm.toLowerCase())>=0);
+    this.setState({feedItmes: searchedItems});
+    event.preventDefault();
+  }
+  public render() {
+   
+    return (
+      <div>
+       <div className="main-content-header">
+         
+         <form onSubmit={this.handleSubmit}>
+          
+             <input
+               type="text"
+               value={this.state.searchTerm}
+               onChange={this.handleChange}
+               placeholder="Search"
+             />
+         
+           <input type="submit" value="Submit" />
+         </form>
+       </div>
 
-        let url= this.props.onlySubscribed? 'api/News/Get?onlySubscribed=true' : 'api/News/Get';    
 
-        if(this.props.providerId>0){
-            url = 'api/News/Get?providerId='+this.props.providerId;
-        }else if (this.props.onlySubscribed) {
-            url = 'api/News/Get?onlySubscribed=true';
-        }else {
-            url ='api/News/Get';
-        }
-        
-       
-        fetch(url)
-            .then(response => response.json() as Promise<any[]>)
-            .then(data => {
-                this.setState({ feedItmes: data, apiResponded : true })
-            });
-        
-    }
-    public render() {
-        console.log(this.state.feedItmes);
-        return <div>
-            {
-               this.state.feedItmes && this.state.feedItmes.length ? this.state.feedItmes.map( (feedItem, index) => (
-                     < NewsFeedItem data={feedItem} key={index} {...({} as RouteComponentProps<any>)} />
-                )) : this.state.apiResponded && this.props.renderIfNoItemsFound && this.props.renderIfNoItemsFound()
-            
-            }
-            
-        </div>;
-    }
+
+        {this.state.feedItmes && this.state.feedItmes.length
+          ? 
+          
+          
+          this.state.feedItmes.map((feedItem, index) => (
+              <NewsFeedItem
+                data={feedItem}
+                key={index}
+                {...{} as RouteComponentProps<any>}
+              />
+            ))
+          : this.state.apiResponded &&
+            this.props.renderIfNoItemsFound &&
+            this.props.renderIfNoItemsFound()}
+      </div>
+    );
+  }
 }
