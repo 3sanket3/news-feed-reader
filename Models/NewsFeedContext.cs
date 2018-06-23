acd637ad83ad8ab3fbc4c15aae00886d723bc058
Sanket Patel <3sanket3@gmail.com>
2018-06-23 15:09:14 +0530

    add Seed Data configs

--- Models/NewsFeedContext.cs
+++ Models/NewsFeedContext.cs
@@ -1,7 +1,11 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace news_feed_reader.Models
@@ -10,9 +14,26 @@ namespace news_feed_reader.Models
     {
         public DbSet<NewsFeedProvider> NewsFeedProviders { get; set; }
 
+        public NewsFeedContext(DbContextOptions options) : base(options) { }
+        
+        public NewsFeedContext() { }
+
+        public static IConfiguration Configuration { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=newsfeed_sanketpatel;Integrated Security=True");
+           
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+                Configuration = builder.Build();
+                optionsBuilder.UseSqlServer(Configuration.GetConnectionString("NewsFeedDatabase"));
+            
         }
-}
+
+
+
+    }
+
+    
 }
--- Startup.cs
+++ Startup.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using news_feed_reader.Models;
 
 namespace news_feed_reader
 {
@@ -22,7 +24,14 @@ namespace news_feed_reader
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<NewsFeedContext>(options =>
+            {
+                //Environment.SetEnvironmentVariable("EnvironmentName", _hostingEnvironment.EnvironmentName.ToLower() == "development" ? "local" : _hostingEnvironment.EnvironmentName);
+                options.UseSqlServer(Configuration.GetConnectionString("NewsFeedDatabase"));
+            });
             services.AddMvc();
+
+            services.AddSingleton<IConfiguration>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +63,58 @@ namespace news_feed_reader
                     name: "spa-fallback",
                     defaults: new { controller = "Home", action = "Index" });
             });
+
+           InitializeAsync(app.ApplicationServices).Wait();
+            
         }
+
+
+        public static async Task InitializeAsync(IServiceProvider service)
+        {
+
+            using (var serviceScope = service.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+
+                var scopeServiceProvider = serviceScope.ServiceProvider;
+                var db = scopeServiceProvider.GetService<NewsFeedContext>();
+
+                db.Database.Migrate();
+                await InsertSeedData(db);
+            }
+        }
+
+        private static async Task InsertSeedData(NewsFeedContext context)
+        {
+            if (context.NewsFeedProviders.Any())
+                return;
+
+            List<NewsFeedProvider> lstNewsFeedProviders = new List<NewsFeedProvider>()
+            {
+                new NewsFeedProvider()
+                {
+                    Name="ABC News",
+                    RssFeedURL="https://abcnews.go.com/abcnews/topstories"
+                },
+                new NewsFeedProvider()
+                {
+                    Name="BBC News",
+                    RssFeedURL="http://feeds.bbci.co.uk/news/world/rss.xml"
+                },
+                new NewsFeedProvider()
+                {
+                    Name="CNN",
+                    RssFeedURL="http://rss.cnn.com/rss/edition.rss"
+
+                    },
+                    new NewsFeedProvider()
+                    {
+                        Name="Times of India",
+                        RssFeedURL="https://timesofindia.indiatimes.com/rssfeedstopstories.cms"
+                    },
+            };
+            context.AddRange(lstNewsFeedProviders);
+            await context.SaveChangesAsync();
+        }
+        
     }
 }
