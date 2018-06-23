dfe49db277fd824a7399eced5f5ffe775aac567e
Sanket Patel <3sanket3@gmail.com>
2018-06-23 15:08:55 +0530

    rename Pulling feeds to Fetching feeds

--- ClientApp/components/NewsFeedList.tsx
+++ ClientApp/components/NewsFeedList.tsx
@@ -97,7 +97,7 @@ export class NewsFeedList extends React.Component<
           this.props.renderIfNoItemsFound()
         )}
         {
-            !this.state.apiResponded ? (<div className="text-center font-x-large"> ⏳ Pulling feeds... ⏳ </div>) : null
+            !this.state.apiResponded ? (<div className="text-center font-x-large"> ⏳ Fetching feeds... ⏳ </div>) : null
         }
       </div>
     );
