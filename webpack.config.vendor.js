ac82e551221cd7529fbaad9e98a6c6e4073d0102
Sanket Patel <3sanket3@gmail.com>
2018-06-24 19:03:23 +0530

    update webpack config

--- webpack.config.vendor.js
+++ webpack.config.vendor.js
@@ -36,7 +36,7 @@ module.exports = (env) => {
                 'process.env.NODE_ENV': isDevBuild ? '"development"' : '"production"'
             })
         ].concat(isDevBuild ? [] : [
-            new webpack.optimize.UglifyJsPlugin()
+            //new webpack.optimize.UglifyJsPlugin()
         ])
     }];
 };
